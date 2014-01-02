cc6fe4869615b95ab51cf72c36adf34acf424c14
Chinara <chinara.isabaeva@gmail.com>
2014-01-02 19:18:38 +0000

    forms.py added years range

--- mybudget/finplanner/forms.py
+++ mybudget/finplanner/forms.py
@@ -5,19 +5,19 @@ from mybudget.finplanner.models import *
 
 
 class ExpensesForm(ModelForm):
-    date = DateField(widget=SelectDateWidget(), initial=datetime.today())
+    date = DateField(widget=SelectDateWidget(years=range(2013, 2023, 1)), initial=datetime.today())
     class Meta:
         model = Expenses
         widgets = {'amount': TextInput}
 
 class IncomesForm(ModelForm):
-    date = DateField(widget=SelectDateWidget(), initial=datetime.today())
+    date = DateField(widget=SelectDateWidget(years=range(2013, 2023, 1)), initial=datetime.today())
     class Meta:
         model = Incomes
         widgets = {'amount': TextInput}
 
 class ReservesForm(ModelForm):
-    date = DateField(widget=SelectDateWidget(), initial=datetime.today())
+    date = DateField(widget=SelectDateWidget(years=range(2013, 2023, 1)), initial=datetime.today())
     class Meta:
         model = Reserves
         widgets = {'amount': TextInput}
