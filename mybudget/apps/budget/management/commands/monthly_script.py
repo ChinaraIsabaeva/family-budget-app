d5dc0b9a6f3d95232e0ba03539ee263de3cd8fd8
chinara <chinara.isabaeva@gmail.com>
2014-10-28 12:41:22 +0000

    fix encoding problems in monthly_script

--- mybudget/apps/budget/management/commands/monthly_script.py
+++ mybudget/apps/budget/management/commands/monthly_script.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.core.management.base import BaseCommand
 from django.db.models import Sum
 from mybudget.apps.budget.models import Envelopes, Incomes, Accounts, RegularMonthlyExpenses
