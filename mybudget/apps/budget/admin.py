a4ab7bfc1b3d54a2cdf282703cffe135cfebfcde
chinara <chinara.isabaeva@gmail.com>
2014-10-28 19:12:53 +0000

    change dashboard and expenses into admin

--- mybudget/apps/budget/admin.py
+++ mybudget/apps/budget/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import RegularMonthlyExpenses, Incomes, Accounts, Envelopes
+from .models import RegularMonthlyExpenses, Incomes, Accounts, Envelopes, Expenses
 
 # Register your models here.
 admin.site.register(RegularMonthlyExpenses)
 admin.site.register(Incomes)
 admin.site.register(Accounts)
 admin.site.register(Envelopes)
+admin.site.register(Expenses)
