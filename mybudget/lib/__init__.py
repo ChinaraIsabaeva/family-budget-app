d30769436208e9952d34e9b2127b9633c82c1d5a
chinara <chinara.isabaeva@gmail.com>
2014-12-03 14:37:54 +0000

    tests

--- mybudget/lib/__init__.py
+++ mybudget/lib/__init__.py
@@ -7,5 +7,17 @@ def disposable_income():
     incomes = Incomes.objects.all().aggregate(total=Sum('amount'))
     regular_expenses = RegularMonthlyExpenses.objects.all().aggregate(total=Sum('amount'))
     envelopes = Envelopes.objects.all().aggregate(total=Sum('monthly_replenishment'))
-    available_amount = incomes['total'] - regular_expenses['total'] - envelopes['total']
+    if incomes['total'] is None:
+        incomes_total = 0
+    else:
+        incomes_total = incomes['total']
+    if regular_expenses['total'] is None:
+        regular_expenses_total = 0
+    else:
+        regular_expenses_total = regular_expenses['total']
+    if envelopes['total'] is None:
+        envelopes_total = 0
+    else:
+        envelopes_total = envelopes['total']
+    available_amount = incomes_total - regular_expenses_total - envelopes_total
     return available_amount
