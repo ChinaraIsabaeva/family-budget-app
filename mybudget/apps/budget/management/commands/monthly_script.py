bd64768ec8d46ed3b447faf39f6180fc15fa6db3
chinara <chinara.isabaeva@gmail.com>
2014-12-28 12:21:47 +0000

    update monthly script

--- mybudget/apps/budget/management/commands/monthly_script.py
+++ mybudget/apps/budget/management/commands/monthly_script.py
@@ -2,6 +2,8 @@
 
 from django.core.management.base import BaseCommand
 from django.db.models import Sum
+from django.db import transaction
+
 from mybudget.apps.budget.models import Envelopes, Incomes, Accounts, RegularMonthlyExpenses
 
 
@@ -13,13 +15,19 @@ class Command(BaseCommand):
         вычитаются все регулярные месячные расходы и месячная сумма наличных конвертов. Это сумма становится
         "текушей суммой" счета.
         """
-
-        envelopes_cash = Envelopes.objects.all().exclude(cash=False).aggregate(total=Sum('monthly_replenishment'))
-        accounts_beginning = Accounts.objects.all().aggregate(total=Sum('current_amount'))
-        regular_expenses = RegularMonthlyExpenses.objects.all().aggregate(total=Sum('amount'))
-        incomes = Incomes.objects.all().aggregate(total=Sum('amount'))
-        accounts_ending = accounts_beginning['total'] + incomes['total'] - regular_expenses['total'] - envelopes_cash['total']
-        account = Accounts.objects.get(id=1)
-        account.current_amount = accounts_ending
-        account.save()
-
+        try:
+            with transaction.atomic():
+                envelopes_cash = Envelopes.objects.all().exclude(cash=False).aggregate(total=Sum('monthly_replenishment'))
+                accounts_beginning = Accounts.objects.all().aggregate(total=Sum('current_amount'))
+                regular_expenses = RegularMonthlyExpenses.objects.all().aggregate(total=Sum('amount'))
+                incomes = Incomes.objects.all().aggregate(total=Sum('amount'))
+                accounts_ending = accounts_beginning['total'] + incomes['total'] - regular_expenses['total'] - envelopes_cash['total']
+                account = Accounts.objects.get(id=1)
+                account.current_amount = accounts_ending
+                account.save()
+                envelopes = Envelopes.objects.all()
+                for envelope in envelopes:
+                    envelope.current_amount = envelope.current_amount + envelope.monthly_replenishment
+                    envelope.save()
+        except:
+            'Something goes wrong!'
