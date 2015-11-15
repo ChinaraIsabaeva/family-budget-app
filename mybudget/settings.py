6b4f3ce4789330a2511d58f094ead413ebfb64d9
chinara <chinara.isabaeva@gmail.com>
2015-11-15 17:51:49 +0000

    edit settings file

--- mybudget/settings.py
+++ mybudget/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_PATH, 'static'),
